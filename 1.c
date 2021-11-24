a91608957ccc2f8ba532fd53cc00db8a46340ea5
FranciscoDenis17 <86362473+FranciscoDenis17@users.noreply.github.com>
2021-11-24 13:34:15 +0000

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 int main()
 {
-	int tabla [2][3] = {{6,4,5}, {2,6,4}};
-	
-	int i,j;
-	
+	int tabla [2][3], i, j;
+	for (int i=0;i<2;i++)
+	{
+		for (int j=0;j<3;j++)
+		{
+			printf ("Ingrese el valor de la fila y la columna: \n", i, j);
+			scanf ("%d", &tabla[i][j]);
+		}
+	}
 	for(i=0;i<2;i++)
 		
 	{
 		
-	
+		
 		for(j=0;j<3;j++)
 		{
 			printf("%d",tabla[i][j]);
