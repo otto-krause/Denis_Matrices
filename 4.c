10af093ae28415ca512bb43f7684ea82b99b3dcb
FranciscoDenis17 <86362473+FranciscoDenis17@users.noreply.github.com>
2021-11-24 14:55:18 +0000

    Update 4.c

--- 4.c
+++ 4.c
@@ -8,7 +8,7 @@ int main()
 	{
 		for(int j=0;j<12;j++)
 		{
-			printf("El conjunto de la linea %d y el grupo %d es: \n",i+1,j+1);
+			printf("Ingrese la linea (1-3) %d y el auto (1-12) %d es: \n",i+1,j+1);
 			scanf("%d",&tabla[i][j]);
 		}
 	}
@@ -19,15 +19,23 @@ int main()
 		suma1=0;
 		for(int j=0;j<12;j++)
 		{
-			printf("El conjunto de la linea %d y el grupo %d es de:%d \n",i+1,j+1,tabla[i][j]);
+			printf("\nLa linea %d y el grupo %d es de:%d \n",i+1,j+1,tabla[i][j]);
 			suma1=suma1+tabla[i][j];
 		}
 		printf("\n");
 		suma2 = suma2 + suma1;
-		printf("El conjunto de la linea %d es de: %d\n",i+1,suma1);
+		printf("La linea %d es de: %d\n",i+1,suma1);
 		
 		
 	}
-	printf("El conjunto total es de: %d",suma2);
+	printf("\nLa linea total es de: %d\n",suma2);
+	{
+
+		printf ("\nLa recaudacion total de las lineas de los autos es: %d\n",i+1,j+1,tabla[i][j]);
+		tabla[0][i]=tabla[0][i]+tabla[i][j];
+	}
+
+
+	
 	return 0;
 }
